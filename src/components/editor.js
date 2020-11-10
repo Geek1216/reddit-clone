6bf7b7c41a95864e4dc348ae36b4c3eb3e9252de
ahmedyasser001 <ahmadyasser7@outlook.com>
2020-11-09 23:25:59 -0800

    code review

--- src/components/editor.js
+++ src/components/editor.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Editor } from 'react-draft-wysiwyg';
 import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-
 const EditorComponent = () => {
     return( 
         <Editor />
